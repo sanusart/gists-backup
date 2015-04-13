16457e5b2af33453f87dada828806b665e51d1d2
Sasha Khamkov <sanusart@gmail.com>
2015-04-14 01:40:14 +0300

    UPDATE: updated to be node module instalable via NPM

--- README.md
+++ README.md
@@ -1,16 +1,20 @@
-# Gist backup script
+# Gist backup
 
-This script will download all gist files to local directory ("gists" by default)
+This package will download all gist files to directories named after gist description.
 
 Pre-run
 ---
 
-Run `npm install`
+Run `[sudo] npm install gist-backup --globall`
 
 Run
 ---
 
-Run `node bkp.js USERNAME PASSWORD`
+`gist-backup <github-username> <github-password> <path/to/backups>` 
+
+or just 
+
+`gist-backup` (will be prompted for username, password and local path to backup directory)
 
 If all went well, you'll see "gists" directory populated with directories named after gist description.
 Gists with similar descriptions will be appended with 'duplicate N' (where N is an incremented number), gists without description will simply be called 'Untitled'.
---
+++ gist-backup.js
@@ -0,0 +1,91 @@
+#!/usr/bin/env node
+
+var rest = require('restler');
+var request = require('request');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var prompt = require('synchro-prompt');
+
+var ghUsername = process.argv[1] || prompt('Your GitHub Username? ');
+var ghPassword = process.argv[2] || prompt('Your GitHub Password? ');
+var savedir = process.argv[3] || prompt('Your GitHub Username? ');
+
+mkdirp(savedir);
+
+function getGists(page) {
+
+    'use strict';
+
+    var options = {
+        username: ghUsername,
+        password: ghPassword,
+        headers: {
+            'User-Agent': 'Gists backup'
+        }
+    };
+
+    rest.get('https://api.github.com/gists?per_page=100&page=' + page, options).on('complete', function (data, response) {
+        var increment = 1;
+        data.forEach(function (gist) {
+            var description = (!gist.description) ? 'Untitled' : gist.description
+                .trim()
+                .replace(/[^a-zA-Z0-9\s]/g, '');
+
+            var dir = savedir + '/' + description;
+
+            try {
+                fs.statSync(dir);
+                dir = dir + ' duplicate ' + increment++;
+                mkdirp(dir, function (error) {
+                    if (error) {
+                        throw error;
+                    } else {
+                        console.log('successfully created ' + dir);
+                    }
+
+                });
+            }
+            catch (err) {
+                mkdirp(dir, function (error) {
+                    if (error) {
+                        console.log('Error: ' + error);
+                    } else {
+                        console.log('successfully created ' + dir);
+                    }
+
+                });
+            }
+
+            for (var file in gist.files) {
+                if (gist.files.hasOwnProperty(file)) {
+                    var raw_url = gist.files[file].raw_url;
+                    var filename = gist.files[file].filename;
+                    request(raw_url).pipe(fs.createWriteStream(dir + '/' + filename, function (error) {
+                        if (error) {
+                            throw error;
+                        } else {
+                            console.log('successfully created ' + dir + '/' + filename);
+                        }
+
+                    }));
+                }
+            }
+        });
+        if (page === 1 && response.headers.link) {
+            var links = response.headers.link.split(',');
+            for (var link in links) {
+                if (links.hasOwnProperty(link)) {
+                    link = links[link];
+                    if (link.indexOf('rel="next') > -1) {
+                        var pages = link.match(/[0-9]+/)[0];
+                    }
+                }
+                for (var p = 2; p < pages; p++) {
+                    getGists(p);
+                }
+            }
+        }
+    });
+}
+
+getGists(1);
